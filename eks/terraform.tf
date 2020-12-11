af97153aedc7e8923bc05cfc70d37b8771835612
Igor Serko <igor@ly.st>
2020-12-11 08:51:04 +0000

    add remote_state
    The README mentions remote state and has instructions to create a remote
    state S3 bucket, however the configuration doesn't actually use it.
    
    This adds it.

--- eks/terraform.tf
+++ eks/terraform.tf
@@ -2,3 +2,12 @@ provider "aws" {
   version = "~> 3.0.0"
   region  = var.aws_region
 }
+
+data "terraform_remote_state" "state" {
+  backend = "s3"
+  config = {
+    bucket = "${var.basename}-terraform-state"
+    key    = "terraform.tfstate"
+    region = var.aws_region
+  }
+}
